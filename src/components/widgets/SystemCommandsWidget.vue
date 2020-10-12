f7147c69cb5f1ec4aa027078658a6976513c8295
Craig Bassett <craig.bassett@gmail.com>
2020-10-12 18:05:41 -0500

    fix: adds confirm dialogs to shutdown and reboot host buttons

--- src/components/widgets/SystemCommandsWidget.vue
+++ src/components/widgets/SystemCommandsWidget.vue
@@ -1,25 +1,52 @@
 <template>
-  <v-card-text>
-    <v-row>
-      <v-col>
-        <v-btn block color="warning" :disabled="printerBusy" @click="sendGcode('RESTART')" class="me-2 mb-2">Restart</v-btn>
-        <v-btn block color="warning" :disabled="printerBusy" @click="sendGcode('FIRMWARE_RESTART')" class="me-2 mb-2">Firmware Restart</v-btn>
-      </v-col>
-      <v-col>
-        <v-btn block color="error" :disabled="printerBusy" @click="hostReboot" class="me-2 mb-2">Host Reboot</v-btn>
-        <v-btn block color="error" :disabled="printerBusy" @click="hostShutdown" class="me-2 mb-2">Host Shutdown</v-btn>
-      </v-col>
-    </v-row>
-  </v-card-text>
+  <div>
+    <v-card-text>
+      <v-row>
+        <v-col>
+          <v-btn block color="warning" :disabled="printerBusy" @click="sendGcode('RESTART')" class="me-2 mb-2">Restart</v-btn>
+          <v-btn block color="warning" :disabled="printerBusy" @click="sendGcode('FIRMWARE_RESTART')" class="me-2 mb-2">Firmware Restart</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn block color="error" :disabled="printerBusy" @click="confirmRebootDialog.open = true" class="me-2 mb-2">Host Reboot</v-btn>
+          <v-btn block color="error" :disabled="printerBusy" @click="confirmShutdownDialog.open = true" class="me-2 mb-2">Host Shutdown</v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <dialog-confirm
+      v-model="confirmRebootDialog.open"
+      @confirm="hostReboot()">
+      <p>Are you sure? This will reboot your host system.</p>
+    </dialog-confirm>
+
+    <dialog-confirm
+      v-model="confirmShutdownDialog.open"
+      @confirm="hostShutdown()">
+      <p>Are you sure? This will shutdown your host system.</p>
+    </dialog-confirm>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
 import UtilsMixin from '@/mixins/utils'
 import { SocketActions } from '@/socketActions'
+import DialogConfirm from '@/components/dialogs/dialogConfirm.vue'
 
-@Component({})
+@Component({
+  components: {
+    DialogConfirm
+  }
+})
 export default class SystemCommandsWidget extends Mixins(UtilsMixin) {
+  confirmRebootDialog = {
+    open: false
+  }
+
+  confirmShutdownDialog = {
+    open: false
+  }
+
   hostReboot () {
     SocketActions.machineReboot()
   }
