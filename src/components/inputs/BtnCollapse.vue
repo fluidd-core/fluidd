fcbe166686934f12ca1d7b5ce73381a38eebecf3
Craig Bassett <craig.bassett@gmail.com>
2020-11-08 10:30:59 -0600

    fix: ensures tool tabs present properly if only one tab is available

---
+++ src/components/inputs/BtnCollapse.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    @click="emitChange(!value)"
+    fab small text>
+    <v-icon v-if="!value">$chevronUp</v-icon>
+    <v-icon v-if="value">$chevronDown</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class BtnCollapse extends Vue {
+  @Prop({ default: false })
+  value!: boolean
+
+  emitChange (value: boolean) {
+    console.log('emitting ', value)
+    this.$emit('input', value)
+  }
+}
+</script>
