fbac2a0463b1357a80ca76ace6801e39d7ab90ae
Craig Bassett <craig.bassett@gmail.com>
2020-12-30 15:30:34 -0600

    refactor: polished update dialog

--- src/components/dialogs/dialogUpdateStatus.vue
+++ src/components/dialogs/dialogUpdateStatus.vue
@@ -5,46 +5,24 @@
     persistent
   >
     <v-card
+      :loading="'primary'"
       color="secondary darken-1">
       <v-card-title>
-        <span class="headline">Component Update</span>
+        <span class="headline">{{ title }}</span>
       </v-card-title>
       <v-card-text>
-        <v-row
-          v-if="updating"
-          class="fill-height"
-          align-content="center"
-          justify="center"
-        >
-          <v-col
-            class="subtitle-1 text-center"
-            cols="12"
-          >
-            Updating, please wait...
-          </v-col>
-          <v-col cols="6">
-            <v-progress-linear
-              indeterminate
-              class="mb-4"
-              color="warning"
-              rounded
-              height="6"
-            ></v-progress-linear>
-          </v-col>
-        </v-row>
-
-        <v-container style="height: 250px;">
+        <div style="height: 250px;">
           <console-widget
             v-if="updateResponses"
             :items="updateResponses"
             readonly
           ></console-widget>
-        </v-container>
+        </div>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
         <slot name="actions">
-          <v-btn color="primary" @click="close" :disabled="updating">Finish</v-btn>
+          <v-btn color="primary" text @click="close" :disabled="updating">{{ buttonTitle }}</v-btn>
         </slot>
       </v-card-actions>
     </v-card>
@@ -80,9 +58,17 @@ export default class DialogUpdateStatus extends Mixins(UtilsMixin) {
 
   get title () {
     if (this.updating) {
+      return 'Updating, please wait...'
+    } else {
+      return 'Updates finished'
+    }
+  }
+
+  get buttonTitle () {
+    if (this.updating) {
       return 'Updating...'
     } else {
-      return 'Finished'
+      return 'Finish'
     }
   }
 
