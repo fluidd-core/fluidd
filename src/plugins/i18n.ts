68ee4fa2423ff0c43b55eb85fbf24f374a287087
1300792971@qq.com <1300792971@qq.com>
2021-02-05 21:48:24 +0800

    Set English as the default language

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -3,7 +3,7 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
-  locale: localStorage.getItem('lang') ? String(localStorage.getItem('lang')) : 'zh',
+  locale: localStorage.getItem('lang') ? String(localStorage.getItem('lang')) : 'en',
   messages: {
     zh: require('@/assets/languages/zh.json'),
     en: require('@/assets/languages/en.json')
