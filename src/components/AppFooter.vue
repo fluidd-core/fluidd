78ebf72dac0c9a78e70eceab144aee547db056e2
Craig Bassett <craig.bassett@gmail.com>
2021-03-13 19:36:57 -0600

    chore: fix error for app versions in footer
    Signed-off-by: Craig Bassett <craig.bassett@gmail.com>

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,8 +3,8 @@
     <span class="font-weight-light grey--text">
       &copy; {{ new Date().getFullYear() }}
       fluidd: v{{ appInfo.fluidd.version }}-{{ appInfo.fluidd.hash }},
-      moonraker: {{ appInfo.components.moonraker.current_hash }},
-      klipper: {{ appInfo.components.klipper.current_hash }}
+      moonraker: {{ appInfo.moonraker.current_hash }},
+      klipper: {{ appInfo.klipper.current_hash }}
     </span>
     <!-- <pre>{{ appInfo }}</pre> -->
   </v-footer>
@@ -16,15 +16,12 @@ import { Component } from 'vue-property-decorator'
 
 @Component({})
 export default class Footer extends Vue {
-  get clientInfo () {
-    return {
-      version: this.$store.state.version.fluidd.version,
-      hash: this.$store.state.version.fluidd.hash
-    }
-  }
-
   get appInfo () {
-    return this.$store.state.version
+    return {
+      fluidd: this.$store.state.version.fluidd,
+      moonraker: this.$store.state.version.components.moonraker || {},
+      klipper: this.$store.state.version.components.klipper || {}
+    }
   }
 }
 </script>
