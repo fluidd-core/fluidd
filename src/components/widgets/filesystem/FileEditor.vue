ae34d5249dcd503d0fb631491ad5c38918b7315c
Craig Bassett <craig.bassett@gmail.com>
2021-04-08 12:52:28 -0500

    chore: ensure editor has focus on load
    Signed-off-by: Craig Bassett <craig.bassett@gmail.com>

--- src/components/widgets/filesystem/FileEditor.vue
+++ src/components/widgets/filesystem/FileEditor.vue
@@ -108,6 +108,9 @@ export default class FileEditor extends Vue {
     )
     this.editor.setModel(model)
 
+    // Focus the editor.
+    this.editor.focus()
+
     // Fire the editorMounted call.
     this.editorMounted()
   }
