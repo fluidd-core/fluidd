73e03e45c2e69fe099c442afbba1bd45badb9de8
Craig Bassett <craig.bassett@gmail.com>
2021-04-08 13:45:48 -0500

    fix: sets default mesh scale to a more sane value
    Signed-off-by: Craig Bassett <craig.bassett@gmail.com>

--- src/components/widgets/bedmesh/BedMeshChart.vue
+++ src/components/widgets/bedmesh/BedMeshChart.vue
@@ -152,8 +152,6 @@ export default class EChartsBedMesh extends Vue {
         left: 0,
         dimension: 2,
         precision: 4,
-        min: -0.1,
-        max: 0.1,
         inRange: {
           color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
         },
