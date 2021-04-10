09db18911f8f2f8bf1e1fc873379823f83315701
Craig Bassett <craig.bassett@gmail.com>
2021-04-10 09:59:10 -0500

    feat: link to correct config docs dependent on file being edited
    Signed-off-by: Craig Bassett <craig.bassett@gmail.com>

--- src/globals.ts
+++ src/globals.ts
@@ -130,7 +130,8 @@ export const Globals = Object.freeze({
   DOCS_REQUIRED_CONFIGURATION: 'https://docs.fluidd.xyz/configuration/initial_setup',
   DOCS_MULTIPLE_INSTANCES: 'https://docs.fluidd.xyz/configuration/multiple_printers',
   DOCS_MOONRAKER_PLUGINS: 'https://docs.fluidd.xyz/configuration/moonraker',
-  DOCS_KLIPPER_CONFIG_REF: 'https://www.klipper3d.org/Config_Reference.html'
+  DOCS_KLIPPER_CONFIG_REF: 'https://www.klipper3d.org/Config_Reference.html',
+  DOCS_MOONRAKER_CONFIG_REF: 'https://moonraker.readthedocs.io/en/latest/configuration/'
 })
 
 export const Icons = Object.freeze({
