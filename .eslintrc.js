d1f50a536b220c255289d1fbf63e0a1fc006c7a5
Mechazawa <mega@ioexception.at>
2021-04-24 23:05:00 +0200

    Fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
-  ignorePatterns: [
-    // Transpiled before they are analysed
-    '*.worker.js'
-  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
