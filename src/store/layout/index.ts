bfba30192add06585e8e36a7ffd0fa9f27ff5442
Craig Bassett <craig.bassett@gmail.com>
2021-05-21 11:14:58 -0500

    feat: re-enable g-code preview
    Signed-off-by: Craig Bassett <craig.bassett@gmail.com>

--- src/store/layout/index.ts
+++ src/store/layout/index.ts
@@ -27,8 +27,8 @@ export const defaultState = (): LayoutState => {
           { id: 'temperature-card', enabled: true, collapsed: false },
           { id: 'macros-card', enabled: true, collapsed: false },
           { id: 'jobs-card', enabled: true, collapsed: false },
-          { id: 'console-card', enabled: true, collapsed: false }
-          // { id: 'gcode-preview-card', enabled: true, collapsed: false }
+          { id: 'console-card', enabled: true, collapsed: false },
+          { id: 'gcode-preview-card', enabled: true, collapsed: false }
         ]
       }
     }
