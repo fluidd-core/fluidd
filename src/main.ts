5ba5160fb8586c8dcbbc84a585db48c8249c494e
Pedro Lamas <pedrolamas@gmail.com>
2022-03-01 20:27:19 +0000

    fix: attempt to fix temp chart issues (#551)
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -33,11 +33,10 @@ import { FiltersPlugin } from './plugins/filters'
 import { SocketPlugin } from './plugins/socketClient'
 import { ColorSetPlugin } from './plugins/colorSet'
 import { DayJSPlugin } from './plugins/dayjs'
-import { plugin } from 'echarts-for-vue'
 
 // Import ECharts
-// import * as echarts from 'echarts'
-import * as echarts from 'echarts/core'
+import ECharts from 'vue-echarts'
+import { use as echartsUse } from 'echarts/core'
 import { LineChart } from 'echarts/charts'
 import { Grid3DComponent } from 'echarts-gl/components'
 import { SurfaceChart } from 'echarts-gl/charts'
@@ -49,7 +48,6 @@ import {
   LegendComponent,
   VisualMapComponent
 } from 'echarts/components'
-
 import { SVGRenderer, CanvasRenderer } from 'echarts/renderers'
 
 // Main App component
@@ -70,7 +68,7 @@ Vue.component('vue-headful', vueHeadful)
 // Use any Plugins
 
 // Configure echarts
-echarts.use([
+echartsUse([
   DatasetComponent,
   TooltipComponent,
   GridComponent,
@@ -83,8 +81,8 @@ echarts.use([
   SVGRenderer,
   CanvasRenderer
 ])
+Vue.component('v-chart', ECharts)
 
-Vue.use(plugin, { echarts })
 Vue.use(VueVirtualScroller)
 Vue.use(DayJSPlugin)
 Vue.use(FiltersPlugin)
