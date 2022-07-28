6d3b730321bf911a74644fd70200a9ce923fd2d3
BastelKlug <44060099+BastelKlug@users.noreply.github.com>
2022-07-28 21:53:21 +0200

    fix: Check Bed_Mesh Support
    Signed-off-by: Kerim Bilgic <bastelklug@pfusch.eu>

--- src/components/widgets/bedmesh/BedMeshCard.vue
+++ src/components/widgets/bedmesh/BedMeshCard.vue
@@ -1,5 +1,6 @@
 <template>
   <collapsable-card
+    v-if="supportsBedMesh && klippyReady"
     :title="$t('app.general.title.bedmesh')"
     :lazy="false"
     icon="$bedMesh"
@@ -164,6 +165,10 @@ export default class BedMeshCard extends Mixins(StateMixin, ToolheadMixin) {
   // The current processed mesh data, if any.
   get mesh () {
     return this.$store.getters['mesh/getCurrentMeshData']
+  }
+
+  get supportsBedMesh () {
+    return this.$store.getters['mesh/getSupportsBedMesh']
   }
 
   get isMobile () {
