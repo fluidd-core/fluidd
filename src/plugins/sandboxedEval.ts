7063ecd6c3fe0c5df27be2ecc23662479352045f
Mathis Mensing <matmen@dreadful.tech>
2022-08-02 19:04:33 +0200

    fix: don't trigger network request on sandboxed eval
    Signed-off-by: Mathis Mensing <matmen@dreadful.tech>

--- src/plugins/sandboxedEval.ts
+++ src/plugins/sandboxedEval.ts
@@ -1,7 +1,6 @@
 export default function (code: string): any {
   const frame = document.createElement('iframe')
 
-  frame.src = 'data:text/javascript;'
   frame.style.display = 'none'
   document.body.appendChild(frame) // needed for contentWindow
 
