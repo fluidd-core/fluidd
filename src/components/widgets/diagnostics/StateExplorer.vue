a9b7524498a0cbd0ce5162eba8911b761d34e6b7
Mathis Mensing <matmen@dreadful.tech>
2022-08-03 12:00:03 +0200

    fix: state explorer
    Signed-off-by: Mathis Mensing <matmen@dreadful.tech>

---
+++ src/components/widgets/diagnostics/StateExplorer.vue
@@ -0,0 +1,41 @@
+<template>
+  <json-viewer
+    :value="state"
+    :expand-depth="2"
+    :class="$vuetify.theme.isDark ? 'jv-dark' : ''"
+    sort
+  />
+</template>
+
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator'
+import StateMixin from '@/mixins/state'
+import JsonViewer from 'vue-json-viewer'
+
+@Component({
+  components: { JsonViewer }
+})
+export default class StateExplorer extends Mixins(StateMixin) {
+  get state () {
+    return {
+      printer: this.$store.state.printer.printer
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.jv-container.jv-dark {
+  background: transparent;
+  color: rgba(255, 255, 255, 0.8);
+
+  .jv-item.jv-object { color: rgba(255, 255, 255, 0.8); }
+  .jv-item.jv-array { color: rgba(255, 255, 255, 0.8); }
+  .jv-key { color: rgba(255, 255, 255, 0.8); }
+
+  .jv-ellipsis {
+    color: rgba(255, 255, 255, 0.5);
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+}
+</style>
