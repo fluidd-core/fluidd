cfb3da5357026ad1da2cbd8638caac77839d09d9
Pedro Lamas <pedrolamas@gmail.com>
2022-10-12 07:51:02 +0100

    Update src/components/widgets/filesystem/FileSystemContextMenu.vue
    Co-authored-by: Mathis Mensing <github@matmen.dev>

--- src/components/widgets/filesystem/FileSystemContextMenu.vue
+++ src/components/widgets/filesystem/FileSystemContextMenu.vue
@@ -27,7 +27,7 @@
               <v-list-item-icon>
                 <v-icon>$printer</v-icon>
               </v-list-item-icon>
-              <v-list-item-title> {{ $t('app.general.btn.print') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('app.general.btn.print') }}</v-list-item-title>
             </v-list-item>
             <v-list-item
               v-if="canPreheat"
