36952e232094750943fdd7c1310c3eb932ce0279
Pedro Lamas <pedrolamas@gmail.com>
2022-10-25 22:11:41 +0100

    clean up comments

--- src/components/widgets/filesystem/setupMonaco.ts
+++ src/components/widgets/filesystem/setupMonaco.ts
@@ -1,19 +1,13 @@
 import 'monaco-editor/esm/vs/editor/editor.all.js'
 
+// full list of features on 'monaco-editor/esm/metadata.js'
 import 'monaco-editor/esm/vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp.js'
 import 'monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js'
-// import 'monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js'
-// import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess.js'
 import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.js'
-// import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess.js'
 import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js'
-// import 'monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch.js'
 
-// import 'monaco-editor/esm/vs/language/typescript/monaco.contribution'
 import 'monaco-editor/esm/vs/language/css/monaco.contribution'
 import 'monaco-editor/esm/vs/language/json/monaco.contribution'
-// import 'monaco-editor/esm/vs/language/html/monaco.contribution'
-// import 'monaco-editor/esm/vs/basic-languages/monaco.contribution'
 import 'monaco-editor/esm/vs/basic-languages/css/css.contribution'
 import 'monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution'
 
