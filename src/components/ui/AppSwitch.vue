15ddb4882248d049414f6da7426c1c3cc17b3012
Pedro Lamas <pedrolamas@gmail.com>
2023-02-06 16:06:56 +0000

    refactor: new AppSelect and PromiseWrapper components
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/components/ui/AppSwitch.vue
+++ src/components/ui/AppSwitch.vue
@@ -6,14 +6,14 @@
     <div
       class="text-body-1"
       :class="{ 'text--disabled': disabled }"
-    >
-      {{ label }}
-    </div>
+      v-html="label"
+    />
     <v-switch
       v-model="inputValue"
       class="mt-0"
       :disabled="disabled || loading"
       hide-details
+      v-on="$listeners"
     />
   </v-layout>
 </template>
