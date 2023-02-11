8ed7826a1050ff8508d5908b7347fe3413b946a6
Pedro Lamas <pedrolamas@gmail.com>
2023-02-11 17:05:10 +0000

    feat: dragging jobs to job queue
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/util/merge-file-update.ts
+++ src/util/merge-file-update.ts
@@ -1,15 +1,14 @@
 import getFilePaths from './get-file-paths'
-import {
-  AppFile,
-  AppFileWithMeta
-} from '@/store/files/types'
+import { AppFile } from '@/store/files/types'
 
 /**
  * File Updates come in with the filename representing the filepath,
  * so we need to strip the path to reflect what we store.
  */
-export default (root: string, existing: AppFile | AppFileWithMeta, updates: AppFile | AppFileWithMeta): AppFileWithMeta | AppFile => {
+const mergeFileUpdate = <T extends AppFile>(root: string, existing: T, updates: T): T => {
   const paths = getFilePaths(updates.filename, root)
   updates.filename = paths.filename
   return { ...existing, ...updates }
 }
+
+export default mergeFileUpdate
