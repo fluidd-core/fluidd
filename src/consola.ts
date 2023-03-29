690e243fc5d24bbab23a188d4e5c5573eb78c29d
Pedro Lamas <pedrolamas@gmail.com>
2023-03-29 16:26:38 +0100

    chore: dependency updates
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/consola.ts
+++ src/consola.ts
@@ -1,7 +1,7 @@
-import Consola from 'consola'
+import Consola, { LogLevel } from 'consola'
 
 // Configure Consola
 Consola.wrapAll()
 Consola.level = 1
 
-if (import.meta.env.DEV) Consola.level = 6
+if (import.meta.env.DEV) Consola.level = LogLevel.Verbose
