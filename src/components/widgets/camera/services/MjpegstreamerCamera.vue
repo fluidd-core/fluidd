c0276ab414fab1c849ac8bb81cdfb77672bb0447
Pedro Lamas <pedrolamas@gmail.com>
2023-07-28 13:02:21 +0100

    refactor: move each camera to its own component (#1129)
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

---
+++ src/components/widgets/camera/services/MjpegstreamerCamera.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    ref="streamingElement"
+    :src="cameraImageSource"
+    :style="cameraStyle"
+  >
+</template>
+
+<script lang="ts">
+import { Component, Mixins, Ref } from 'vue-property-decorator'
+import CameraMixin from '@/mixins/camera'
+
+@Component({})
+export default class MjpegstreamerCamera extends Mixins(CameraMixin) {
+  @Ref('streamingElement')
+  readonly cameraImage!: HTMLImageElement
+
+  cameraImageSource = ''
+
+  startPlayback () {
+    const url = new URL(this.cameraUrl)
+
+    if (!url.searchParams.get('action')?.startsWith('stream')) {
+      url.searchParams.set('action', 'stream')
+    }
+
+    url.searchParams.set('cacheBust', Date.now().toString())
+
+    this.cameraImageSource = url.toString()
+
+    this.$emit('raw-camera-url', this.cameraImageSource)
+  }
+
+  stopPlayback () {
+    this.cameraImageSource = ''
+    this.cameraImage.src = ''
+  }
+}
+</script>
