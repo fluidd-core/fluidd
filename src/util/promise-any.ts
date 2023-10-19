d813ad1eeab73b833d7dc85454aafddc00d70f72
Pedro Lamas <pedrolamas@gmail.com>
2023-10-19 09:55:21 +0100

    refactor: use native Promise.any() if available
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/util/promise-any.ts
+++ src/util/promise-any.ts
@@ -1,4 +1,8 @@
 const promiseAny = <T>(iterable: Iterable<T | PromiseLike<T>>): Promise<T> => {
+  if ('any' in Promise && typeof Promise.any === 'function') {
+    return Promise.any(iterable)
+  }
+
   return Promise.all(
     [...iterable].map(promise => {
       return new Promise((resolve, reject) =>
