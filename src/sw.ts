fc017922bb44c3d3a04694a87e6a2095d0279caf
Pedro Lamas <pedrolamas@gmail.com>
2023-12-13 20:19:56 +0000

    refactor: simplify service worker code
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/sw.ts
+++ src/sw.ts
@@ -3,7 +3,6 @@
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute, PrecacheFallbackPlugin } from 'workbox-precaching'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
 import { StaleWhileRevalidate } from 'workbox-strategies'
-import escapeRegExp from '@/util/escape-regexp'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -13,15 +12,8 @@ self.addEventListener('message', (event) => {
   }
 })
 
-const swPath = self.location.pathname
-const swPathIndex = swPath.lastIndexOf('/')
-
-const baseUrl = swPathIndex >= 0
-  ? swPath.substring(0, swPathIndex + 1)
-  : '/'
-
 registerRoute(
-  `${import.meta.env.BASE_URL}config.json`,
+  'config.json',
   new StaleWhileRevalidate({
     cacheName: 'config',
     fetchOptions: {
@@ -29,7 +21,7 @@ registerRoute(
     },
     plugins: [
       new PrecacheFallbackPlugin({
-        fallbackURL: `${import.meta.env.BASE_URL}config.json`
+        fallbackURL: 'config.json'
       })
     ]
   }),
@@ -40,22 +32,20 @@ precacheAndRoute(self.__WB_MANIFEST)
 
 cleanupOutdatedCaches()
 
-const escapedBaseUrl = escapeRegExp(baseUrl)
-
 const denylist = import.meta.env.DEV
   ? undefined
   : [
-      'websocket',
-      '(printer|api|access|machine|server)/',
-      'webcam[2-4]?/'
-    ].map(item => new RegExp(`^${escapedBaseUrl}${item}`))
+      /\/websocket/,
+      /\/(printer|api|access|machine|server)\//,
+      /\/webcam[2-4]?\//
+    ]
 
 const allowlist = import.meta.env.DEV
   ? [/^\/$/]
   : undefined
 
 registerRoute(
-  new NavigationRoute(createHandlerBoundToURL(`${import.meta.env.BASE_URL}index.html`), {
+  new NavigationRoute(createHandlerBoundToURL('index.html'), {
     allowlist,
     denylist
   })
