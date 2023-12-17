eb12ff1a23e1b9dcac7c9b9a284dd7ca5c4e7195
vajonam <152501+vajonam@users.noreply.github.com>
2023-12-17 17:48:42 -0500

    Update FileSystem.vue
    Co-authored-by: Pedro Lamas <pedrolamas@gmail.com>
    Signed-off-by: vajonam <152501+vajonam@users.noreply.github.com>

--- src/components/widgets/filesystem/FileSystem.vue
+++ src/components/widgets/filesystem/FileSystem.vue
@@ -394,7 +394,7 @@ export default class FileSystem extends Mixins(StateMixin, FilesMixin, ServicesM
             }
             break
           case 'moonraker_backup_files':
-            if (file.type === 'file' && file.filename.match(/^.moonraker.conf.bkp$/)) { return false }
+            if (file.type === 'file' && file.filename === 'moonraker.conf.bkp') { return false }
             break
           case 'klipper_backup_files':
             if (file.type === 'file' && file.filename.match(/^printer-\d{8}_\d{6}\.cfg$/)) {
