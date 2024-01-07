ccf76202ab11aaa9e9a74b0bc641a80fd918368d
Mathis Mensing <github@matmen.dev>
2024-01-07 20:48:27 +0100

    feat(spoolman): live update support
    Signed-off-by: Mathis Mensing <github@matmen.dev>

--- src/store/spoolman/actions.ts
+++ src/store/spoolman/actions.ts
@@ -1,5 +1,5 @@
 import type { ActionTree } from 'vuex'
-import type { SpoolmanState } from './types'
+import type { Spool, SpoolmanState } from './types'
 import type { RootState } from '../types'
 import { SocketActions } from '@/api/socketActions'
 
@@ -21,6 +21,31 @@ export const actions: ActionTree<SpoolmanState, RootState> = {
 
   async onActiveSpool ({ commit }, payload) {
     commit('setActiveSpool', payload.spool_id)
+  },
+
+  async onSpoolChange ({ commit, getters }, { type, payload }) {
+    const spools = getters.getAvailableSpools
+    switch (type) {
+      case 'added': {
+        spools.push(payload)
+        break
+      }
+
+      case 'updated': {
+        const index = spools.findIndex((spool: Spool) => spool.id === payload.id)
+        spools[index] = payload
+
+        break
+      }
+
+      case 'deleted': {
+        const index = spools.findIndex((spool: Spool) => spool.id === payload.id)
+        spools.splice(index, 1)
+        break
+      }
+    }
+
+    commit('setAvailableSpools', spools)
   },
 
   async onAvailableSpools ({ commit }, payload) {
--- src/store/spoolman/mutations.ts
+++ src/store/spoolman/mutations.ts
@@ -1,6 +1,15 @@
 import type { MutationTree } from 'vuex'
 import { defaultState } from './state'
-import type { Spool, SpoolmanState, SpoolSelectionDialogState } from '@/store/spoolman/types'
+import type {
+  Spool,
+  SpoolmanState,
+  SpoolSelectionDialogState,
+  WebsocketPayload
+} from '@/store/spoolman/types'
+import store from '@/store'
+import { consola } from 'consola'
+
+const logPrefix = '[spoolman]'
 
 export const mutations: MutationTree<SpoolmanState> = {
   /**
@@ -16,7 +25,46 @@ export const mutations: MutationTree<SpoolmanState> = {
 
   setAvailableSpools (state, payload: Spool[]) {
     // implies working communication with spoolman server
-    state.supported = !!payload.length // spools available
+    const supported = !!payload.length // spools available
+
+    if (supported && store.state.server.config.spoolman?.server) {
+      // init websocket to listen for updates
+      const spoolmanUrl = new URL(store.state.server.config.spoolman?.server)
+      spoolmanUrl.pathname += `${spoolmanUrl.pathname.endsWith('/') ? '' : '/'}api/v1/spool`
+      if (spoolmanUrl.protocol === 'https:') {
+        spoolmanUrl.protocol = 'wss:'
+      } else {
+        spoolmanUrl.protocol = 'ws:'
+      }
+
+      state.socket = new WebSocket(spoolmanUrl)
+      state.socket.onerror = err => consola.warn(`${logPrefix} received websocket error`, err)
+      state.socket.onmessage = event => {
+        let data
+        try {
+          data = JSON.parse(event.data) as WebsocketPayload
+        } catch (err) {
+          consola.error(`${logPrefix} failed to decode websocket message`, err, event.data)
+          return
+        }
+
+        consola.debug(`${logPrefix} received spoolman message:`, data)
+        switch (data.resource) {
+          case 'spool':
+            store.dispatch('spoolman/onSpoolChange', data)
+            break
+
+          default:
+            consola.debug(`${logPrefix} ignoring websocket message with type ${data.resource}`)
+        }
+      }
+    } else {
+      // destroy ws
+      state.socket?.close()
+      state.socket = undefined
+    }
+
+    state.supported = supported
     state.availableSpools = payload.map(spool => ({
       ...spool,
       registered: new Date(spool.registered),
