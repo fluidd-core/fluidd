c2e34d7eb2686d41852cdb1403e9d62e5492e4be
Pedro Lamas <pedrolamas@gmail.com>
2024-02-14 21:36:34 +0000

    feat: adds MCU last stats (#1358)
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

---
+++ src/components/widgets/system/McuInformationDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <app-dialog
+    v-model="open"
+    :title="$t('app.system_info.label.mcu_information', { mcu: mcu.name })"
+    max-width="500"
+    no-actions
+  >
+    <div class="overflow-y-auto">
+      <v-card flat>
+        <v-card-title>{{ $t('app.system_info.label.constants') }}</v-card-title>
+
+        <v-simple-table dense>
+          <tbody>
+            <tr
+              v-for="(value, key) in mcu.mcu_constants"
+              :key="key"
+            >
+              <th>{{ key }}</th>
+              <td>{{ value }}</td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-card>
+
+      <v-card flat>
+        <v-card-title>{{ $t('app.system_info.label.last_stats') }}</v-card-title>
+
+        <v-simple-table dense>
+          <tbody>
+            <tr
+              v-for="(value, key) in mcu.last_stats"
+              :key="key"
+            >
+              <th>{{ key }}</th>
+              <td>{{ value }}</td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-card>
+    </div>
+  </app-dialog>
+</template>
+
+<script lang="ts">
+import type { MCU } from '@/store/printer/types'
+import { Component, Prop, VModel, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class McuInformationDialog extends Vue {
+  @VModel({ type: Boolean })
+    open?: boolean
+
+  @Prop({ type: Object, required: true })
+  readonly mcu!: MCU
+}
+</script>
