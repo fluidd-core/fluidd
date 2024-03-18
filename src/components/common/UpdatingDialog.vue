4d76edb6b0699b08d0068cadf91e007b71577783
Pedro Lamas <pedrolamas@gmail.com>
2024-03-18 12:24:25 +0000

    refactor: use AppDialog in UpdatingDialog
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/components/common/UpdatingDialog.vue
+++ src/components/common/UpdatingDialog.vue
@@ -1,88 +1,73 @@
 <template>
-  <v-dialog
-    :value="showDialog"
+  <app-dialog
+    v-model="open"
+    :title="updating ? $t('app.version.status.updating') : $t('app.version.status.finished')"
+    :loading="updating"
+    :close-button-disabled="updating"
     max-width="650"
     persistent
   >
-    <v-card :loading="(updating) ? 'primary' : false">
-      <v-card-title>
-        <span class="headline">{{ title }}</span>
-      </v-card-title>
-      <v-card-text>
-        <console
-          :items="responses"
-          key-field="id"
-          :height="250"
-          readonly
-        />
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <slot name="actions">
-          <app-btn
-            color="primary"
-            text
-            :disabled="updating"
-            @click="close"
-          >
-            {{ buttonTitle }}
-          </app-btn>
-        </slot>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+    <v-card-text>
+      <console
+        :items="responses"
+        :fullscreen="isMobileViewport"
+        :height="250"
+        readonly
+      />
+    </v-card-text>
+
+    <template #actions>
+      <v-spacer />
+
+      <app-btn
+        color="primary"
+        text
+        :disabled="updating"
+        @click="open = false"
+      >
+        {{ updating ? $t('app.version.status.updating') : $t('app.version.btn.finish') }}
+      </app-btn>
+    </template>
+  </app-dialog>
 </template>
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
 import StateMixin from '@/mixins/state'
 import Console from '@/components/widgets/console/Console.vue'
+import BrowserMixin from '@/mixins/browser'
 
 @Component({
   components: {
     Console
   }
 })
-export default class UpdatingDialog extends Mixins(StateMixin) {
+export default class UpdatingDialog extends Mixins(StateMixin, BrowserMixin) {
   invokedDialog = false
-  get showDialog () {
-    if (
-      this.invokedDialog === true ||
-      this.$store.state.version.busy
-    ) {
+
+  get open (): boolean {
+    if (this.invokedDialog || this.updating) {
       this.invokedDialog = true
+
       return true
     }
+
     return false
+  }
+
+  set open (value: boolean) {
+    if (!value) {
+      this.invokedDialog = false
+      this.$store.commit('version/setClearUpdateResponse')
+    }
   }
 
   get updating () {
     return this.$store.state.version.busy
   }
 
-  get title () {
-    if (this.updating) {
-      return this.$t('app.version.status.updating')
-    } else {
-      return this.$t('app.version.status.finished')
-    }
-  }
-
-  get buttonTitle () {
-    if (this.updating) {
-      return this.$t('app.version.status.updating')
-    } else {
-      return this.$t('app.version.btn.finish')
-    }
-  }
-
   get responses () {
     return this.$store.getters['version/getResponses']
-  }
-
-  close () {
-    this.invokedDialog = false
-    this.$store.commit('version/setClearUpdateResponse')
   }
 }
 </script>
