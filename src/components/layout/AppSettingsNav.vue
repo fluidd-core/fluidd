5029dab0a6fe7b44ab7db55f37a2ba739bfa4e9c
Pedro Lamas <pedrolamas@gmail.com>
2024-04-30 18:32:10 +0100

    feat: backup and restore settings (#1427)
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/components/layout/AppSettingsNav.vue
+++ src/components/layout/AppSettingsNav.vue
@@ -44,7 +44,7 @@ export default class AppSettingsNav extends Vue {
       { name: this.$tc('app.setting.title.camera', 2), hash: '#camera', visible: true },
       { name: this.$t('app.setting.title.tool'), hash: '#toolhead', visible: true },
       { name: this.$t('app.setting.title.thermal_presets'), hash: '#presets', visible: true },
-      { name: this.$t('app.setting.title.gcode_preview'), icon: '$cubeScan', hash: '#gcodePreview', visible: true },
+      { name: this.$t('app.setting.title.gcode_preview'), hash: '#gcodePreview', visible: true },
       { name: this.$t('app.general.title.timelapse'), hash: '#timelapse', visible: this.supportsTimelapse },
       { name: this.$t('app.spoolman.title.spoolman'), hash: '#spoolman', visible: this.supportsSpoolman },
       { name: this.$t('app.version.title'), hash: '#versions', visible: this.supportsVersions }
