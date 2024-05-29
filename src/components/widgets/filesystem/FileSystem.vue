df59f785b25d7ea90245e7e676aef307745d41cb
Pedro Lamas <pedrolamas@gmail.com>
2024-05-29 09:27:11 +0100

    fix(FileSystem): zip timestamp format
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/components/widgets/filesystem/FileSystem.vue
+++ src/components/widgets/filesystem/FileSystem.vue
@@ -940,7 +940,14 @@ export default class FileSystem extends Mixins(StateMixin, FilesMixin, ServicesM
 
   handleCreateZip (file: FileBrowserEntry | FileBrowserEntry[]) {
     const date = new Date()
-    const timestamp = `${date.getFullYear()}${date.getMonth()}${date.getDate()}-${date.getHours()}${date.getMinutes()}${date.getSeconds()}`
+    const year = date.getFullYear().toString()
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const day = date.getDate().toString().padStart(2, '0')
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    const seconds = date.getSeconds().toString().padStart(2, '0')
+    const timestamp = `${year}${month}${day}-${hours}${minutes}${seconds}`
+
     const dest = Array.isArray(file)
       ? `${this.currentPath}/${timestamp}.zip`
       : `${this.currentPath}/${file.name}-${timestamp}.zip`
