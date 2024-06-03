0c4f79ecb705d2c946384fa86620212b95e031bb
Pedro Lamas <pedrolamas@gmail.com>
2024-06-03 18:20:35 +0100

    feat: adds Danger Klipper config support (#1446)
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/globals.ts
+++ src/globals.ts
@@ -227,7 +227,14 @@ export const Globals = Object.freeze({
     { filename: 'mooncord-webcam.json', service: 'webcamd', link: 'https://github.com/eliteSchwein/mooncord' },
     { prefix: 'mooncord', service: 'MoonCord', link: 'https://github.com/eliteSchwein/mooncord' },
     { filename: 'telegram.conf', service: 'moonraker-telegram-bot', link: 'https://github.com/nlef/moonraker-telegram-bot/wiki/Sample-config' },
-    { suffix: '.cfg', service: 'klipper', link: 'https://www.klipper3d.org/Config_Reference.html' }
+    {
+      suffix: '.cfg',
+      service: 'klipper',
+      link: 'https://www.klipper3d.org/Config_Reference.html',
+      linkOverride: {
+        'danger-klipper': 'https://dangerklipper.io/Config_Reference.html'
+      }
+    }
   ],
   FILE_DATA_TRANSFER_TYPES: {
     files: 'x-fluidd-files',
