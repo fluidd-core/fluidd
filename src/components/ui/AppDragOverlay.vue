2716370e79969a153d2655a13a8e6ba71538002b
Pedro Lamas <pedrolamas@gmail.com>
2024-09-19 14:33:46 +0100

    fix: hide drag overlay on mouse drag leave
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/components/ui/AppDragOverlay.vue
+++ src/components/ui/AppDragOverlay.vue
@@ -49,6 +49,7 @@ export default class AppDragOverlay extends Vue {
 <style lang="scss" scoped>
   .dragOverlay.v-overlay--active {
     border: dashed 3px #616161;
+    pointer-events: none !important;
   }
 
   .dragOverlay :deep(.v-overlay__content) {
