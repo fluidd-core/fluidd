f446b32d3c8c1e6ec6ddda01c03e087d1fc672a7
Pedro Lamas <pedrolamas@gmail.com>
2025-01-20 21:35:59 +0000

    feat: Kalico firmware retraction z-hop support
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- src/components/widgets/retract/Retract.vue
+++ src/components/widgets/retract/Retract.vue
@@ -89,6 +89,30 @@
         />
       </v-col>
     </v-row>
+
+    <v-row v-if="supportsZHopHeight">
+      <v-col
+        cols="12"
+        sm="6"
+        md="12"
+        lg="6"
+      >
+        <app-named-slider
+          :label="$t('app.general.label.z_hop_height')"
+          suffix="mm"
+          :value="z_hop_height"
+          :reset-value="defaults.z_hop_height"
+          :min="0"
+          :step="0.1"
+          :max="z_hop_height_max"
+          overridable
+          :disabled="!klippyReady"
+          :locked="isMobileViewport"
+          :loading="hasWait($waits.onSetZHopHeight)"
+          @submit="setZHopHeight"
+        />
+      </v-col>
+    </v-row>
   </v-card-text>
 </template>
 
@@ -96,6 +120,7 @@
 import { Component, Mixins } from 'vue-property-decorator'
 import StateMixin from '@/mixins/state'
 import BrowserMixin from '@/mixins/browser'
+import type { KlippyApp } from '@/store/printer/types'
 
 @Component({})
 export default class Retract extends Mixins(StateMixin, BrowserMixin) {
@@ -135,8 +160,25 @@ export default class Retract extends Mixins(StateMixin, BrowserMixin) {
     return Math.round(this.defaults.unretract_extra_length * 2 * 100) / 100
   }
 
+  get z_hop_height () {
+    return this.$store.state.printer.printer.firmware_retraction.z_hop_height
+  }
+
+  get z_hop_height_max () {
+    if (this.defaults.z_hop_height <= 0) return 2
+    return Math.round(this.defaults.z_hop_height * 2 * 100) / 100
+  }
+
   get defaults () {
     return this.$store.getters['printer/getPrinterSettings']('firmware_retraction') || {}
+  }
+
+  get klippyApp (): KlippyApp {
+    return this.$store.getters['printer/getKlippyApp'] as KlippyApp
+  }
+
+  get supportsZHopHeight () {
+    return this.klippyApp.isKalicoOrDangerKlipper
   }
 
   setRetractLength (val: number) {
@@ -153,6 +195,10 @@ export default class Retract extends Mixins(StateMixin, BrowserMixin) {
 
   setUnRetractExtraLength (val: number) {
     this.sendGcode(`SET_RETRACTION UNRETRACT_EXTRA_LENGTH=${val}`, this.$waits.onSetUnretractExtraLength)
+  }
+
+  setZHopHeight (val: number) {
+    this.sendGcode(`SET_RETRACTION Z_HOP_HEIGHT=${val}`, this.$waits.onSetZHopHeight)
   }
 }
 </script>
